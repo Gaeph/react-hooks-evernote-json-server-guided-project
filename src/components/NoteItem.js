02e243f21213d0848b3bd8c6f0b10f2b44300678
Gaeph-Phico de Freud JN PHILIPPE <107807964+Gaeph@users.noreply.github.com>
2025-11-03 16:55:51 -0500

    Update NoteItem.js

--- src/components/NoteItem.js
+++ src/components/NoteItem.js
@@ -1,12 +1,18 @@
-
-
 import React from "react";
 
-function NoteItem({ note, onClick }) {
+function NoteItem({ note, onClick, isSelected }) {
+  // Truncate body si plis pase 30 karaktè
+  const truncatedBody =
+    note.body.length > 30 ? `${note.body.substring(0, 30)}...` : note.body;
+
   return (
-    <li className="note-item" onClick={onClick}>
+    <li
+      className={`note-item ${isSelected ? "selected" : ""}`}
+      onClick={onClick}
+      title={note.body} // montre tout kontni lè hover
+    >
       <h2>{note.title}</h2>
-      <p>{note.body.length > 30 ? `${note.body.substring(0, 30)}...` : note.body}</p>
+      <p>{truncatedBody}</p>
     </li>
   );
 }
