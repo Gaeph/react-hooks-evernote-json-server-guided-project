5e61425156823bd6e2138b04425e5638ca34b4dc
Gaeph-Phico de Freud JN PHILIPPE <107807964+Gaeph@users.noreply.github.com>
2025-11-03 16:50:57 -0500

    Refactor Content component for cleaner rendering

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,32 +1,29 @@
-
 import React from "react";
 import NoteEditor from "./NoteEditor";
 import NoteViewer from "./NoteViewer";
 import Instructions from "./Instructions";
 
 function Content({ selectedNote, isEditing, onEditNote, onSaveNote, onCancelEdit }) {
-  const getContent = () => {
-    if (isEditing && selectedNote) {
-      return (
-        <NoteEditor
-          note={selectedNote}
-          onSaveNote={onSaveNote}
-          onCancelEdit={onCancelEdit}
-        />
-      );
-    } else if (selectedNote) {
-      return (
-        <NoteViewer
-          note={selectedNote}
-          onEditNote={onEditNote}
-        />
-      );
-    } else {
-      return <Instructions />;
-    }
-  };
-
-  return <div className="master-detail-element detail">{getContent()}</div>;
+  return (
+    <div className="master-detail-element detail">
+      {selectedNote ? (
+        isEditing ? (
+          <NoteEditor
+            note={selectedNote}
+            onSaveNote={onSaveNote}
+            onCancelEdit={onCancelEdit}
+          />
+        ) : (
+          <NoteViewer
+            note={selectedNote}
+            onEditNote={onEditNote}
+          />
+        )
+      ) : (
+        <Instructions />
+      )}
+    </div>
+  );
 }
 
 export default Content;
